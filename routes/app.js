38151211fc364122a173bf2d76f958b6a0c9ab7a
Chris Wilkinson <chris@chriswilkinson.info>
2017-03-29 18:23:01 +0100

    Start of the frontend stuff

--- routes/app.js
+++ routes/app.js
@@ -1,29 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
-var User = require('../models/user');
-
 router.get('/', function (req, res, next) {
-    User.findOne({}, function(err, doc) {
-        if (err) {
-            return res.send('Error');
-        }
-        res.render('node', {email: doc.email});
-    });
+    res.render('index');
 });
 
-router.post('/', function(req, res, next) {
-   var email = req.body.email;
-   var user = new User({
-       firstName: 'Chris',
-       lastName: 'WIlkinson',
-       password: 'secret',
-       email: email
-   });
-   user.save(function(err){
-       console.log(err);
-    });
-   res.redirect('/');
-});
+
 
 module.exports = router;
