4d67ba4f7dd6300b7c17ac441fe660cd6a7bb8ff
Chris Wilkinson <chris@chriswilkinson.info>
2017-03-30 16:20:50 +0100

    Directives and binding

--- assets/app/app.component.ts
+++ assets/app/app.component.ts
@@ -3,25 +3,7 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'my-app',
     templateUrl: './app.component.html',
-    styles: [`
-        .author {
-            display: inline-block;
-            font-style: italic;
-            font-size: 12px;
-            width: 80%;
-        }
-        .config {
-            display: inline-block;
-            text-align: right;
-            font-size: 12px;
-            width: 19%;
-        }
-    `]
 })
 export class AppComponent {
     content = 'Some Content';
-    message = {
-        content: 'A message',
-        author: 'Chris'
-    };
 }
